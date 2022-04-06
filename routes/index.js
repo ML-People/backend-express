542a3790a14e7df83d91972eecadc6c2862c3d47
Vineet Marri <vmarri25@vt.edu>
2022-04-06 12:09:27 -0400

    task1_update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var router = express.Router();
+let jsObj = { test: 'test', number: 1 };
+
+/**
+ * This was an attempt from before that I had a question about 
+export const getObj = (req, res) => { 
+    res.json(jsObj); 
+}; 
+ */
+
+//'GET' route for object 
+router.get('/testing', function (req, res, next) {
+  res.json(jsObj);
+});
+
+router.put('/testing', function (req, res, next) {
+  jsObj.test = req.body.test; 
+  jsObj.number = req.body.number; 
+
+  //prints out the json object postman sends to the server
+  //what is this line doing? 
+  console.log(req.body);
+
+  //sends back a response to the client
+  res.json(jsObj);
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
--- routes/objRoute.js
+++
@@ -1,9 +0,0 @@
-import {getObj} from '../controllers/objController'; 
-
-const routes = (app) => { 
-    app.route('/testing') 
-    // GET Endpoint 
-    .get(getObj); 
-}
-
-export default routes; 
